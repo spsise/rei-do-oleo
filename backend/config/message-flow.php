6b17b8d88ad8f92b56478c31fc3045f2dd4ff46d
Sebastiao Apolinario <spsise@gmail.com>
2025-08-27 02:46:08 -0300

    🐘 Backend 🔧 chore: Desabilita o rastreamento de fluxo de mensagens, ajustando a configuração para melhorar a flexibilidade na monitorização do sistema.

--- backend/config/message-flow.php
+++ backend/config/message-flow.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'enabled' => env('MESSAGE_FLOW_TRACKING_ENABLED', true),
+    'enabled' => env('MESSAGE_FLOW_TRACKING_ENABLED', false),
 
     /*
     |--------------------------------------------------------------------------
